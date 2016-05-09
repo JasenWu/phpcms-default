a51445a9b7cbcefdd2ecd36717de281cd5b208fb
Jason Wu <447124329@qq.com>
2016-05-09 14:34:26 +0800

    system config

---
+++ caches/caches_tpl_data/caches_data/c11c0609e2a132f53224630c097f5e78.cache.php
@@ -0,0 +1,30 @@
+<?php
+return array (
+  1 => 
+  array (
+    'id' => '1',
+    'catid' => '12',
+    'typeid' => '0',
+    'title' => 'products1',
+    'style' => '',
+    'thumb' => 'http://localhost/taomujiannew/uploadfile/2016/0509/20160509014430951.jpeg',
+    'keywords' => 'products1 keywords',
+    'description' => 'products1 contents',
+    'posids' => '0',
+    'url' => 'http://localhost/taomujiannew/index.php?m=content&c=index&a=show&catid=12&id=1',
+    'listorder' => '0',
+    'status' => '99',
+    'sysadd' => '1',
+    'islink' => '0',
+    'username' => 'phpcms',
+    'inputtime' => '1462600810',
+    'updatetime' => '1462774922',
+    'hitsid' => 'c-1-1',
+    'views' => '18',
+    'yesterdayviews' => '0',
+    'dayviews' => '16',
+    'weekviews' => '16',
+    'monthviews' => '18',
+  ),
+);
+?>
--- caches/configs/system.php
+++ caches/configs/system.php
@@ -1,7 +1,7 @@
 <?php
 return array(
 //网站路径
-'web_path' => '/taomujiannew/',
+'web_path' => '/phpcms-default/',
 //Session配置
 'session_storage' => 'mysql',
 'session_ttl' => 1800,
@@ -21,13 +21,13 @@ return array(
 
 //附件相关配置
 'upload_path' => PHPCMS_PATH.'uploadfile/',
-'upload_url' => 'http://localhost/taomujiannew/uploadfile/', //附件路径
+'upload_url' => 'http://localhost/phpcms-default/uploadfile/', //附件路径
 'attachment_stat' => '1',//是否记录附件使用状态 0 统计 1 统计， 注意: 本功能会加重服务器负担
 
-'js_path' => 'http://localhost/taomujiannew/statics/js/', //CDN JS
-'css_path' => 'http://localhost/taomujiannew/statics/css/', //CDN CSS
-'img_path' => 'http://localhost/taomujiannew/statics/images/', //CDN img
-'app_path' => 'http://localhost/taomujiannew/',//动态域名配置地址
+'js_path' => 'http://localhost/phpcms-default/statics/js/', //CDN JS
+'css_path' => 'http://localhost/phpcms-default/statics/css/', //CDN CSS
+'img_path' => 'http://localhost/phpcms-default/statics/images/', //CDN img
+'app_path' => 'http://localhost/phpcms-default/',//动态域名配置地址
 
 'charset' => 'utf-8', //网站字符集
 'timezone' => 'Etc/GMT-8', //网站时区（只对php 5.1以上版本有效），Etc/GMT-8 实际表示的是 GMT+8
@@ -44,7 +44,7 @@ return array(
 
 'phpsso' => '1',	//是否使用phpsso
 'phpsso_appid' => '1',	//应用id	
-'phpsso_api_url' => 'http://localhost/taomujiannew/phpsso_server',	//接口地址
+'phpsso_api_url' => 'http://localhost/phpcms-default/phpsso_server',	//接口地址
 'phpsso_auth_key' => '6saxvxN4FfBKnDrgGH9Oar1uUyloOxQU', //加密密钥
 'phpsso_version' => '1', //phpsso版本
 
