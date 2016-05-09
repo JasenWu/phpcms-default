820df862fb8b630563654db500be421d5c5cfe30
Jason Wu <447124329@qq.com>
2016-05-09 16:26:58 +0800

    web frame inclued admin

---
+++ caches/caches_tpl_data/caches_data/90ea156cf928e8a4063a7c4398ac991d.cache.php
@@ -0,0 +1,37 @@
+<?php
+return array (
+  1 => 
+  array (
+    'id' => '1',
+    'catid' => '7',
+    'typeid' => '0',
+    'title' => 'downloads',
+    'style' => '',
+    'thumb' => '',
+    'keywords' => 'downloadsdownloads',
+    'description' => 'downloads',
+    'posids' => '0',
+    'url' => 'http://localhost/taomujiannew/index.php?m=content&c=index&a=show&catid=7&id=1',
+    'listorder' => '0',
+    'status' => '99',
+    'sysadd' => '1',
+    'islink' => '0',
+    'username' => 'phpcms',
+    'inputtime' => '1462601308',
+    'updatetime' => '1462601417',
+    'systems' => 'Win2000/WinXP/Win2003',
+    'copytype' => '免费版',
+    'language' => '英文',
+    'classtype' => '',
+    'version' => '',
+    'filesize' => '未知',
+    'stars' => '',
+    'hitsid' => 'c-2-1',
+    'views' => '1',
+    'yesterdayviews' => '0',
+    'dayviews' => '1',
+    'weekviews' => '1',
+    'monthviews' => '1',
+  ),
+);
+?>
---
+++ caches/caches_tpl_data/caches_data/982e4c51ff687a96fa5cf2e1e7dafda6.cache.php
@@ -0,0 +1,30 @@
+<?php
+return array (
+  2 => 
+  array (
+    'id' => '2',
+    'catid' => '6',
+    'typeid' => '0',
+    'title' => 'contents',
+    'style' => '',
+    'thumb' => 'http://localhost/taomujiannew/uploadfile/2016/0509/20160509013833270.jpeg',
+    'keywords' => 'contents',
+    'description' => 'contents',
+    'posids' => '0',
+    'url' => 'http://localhost/taomujiannew/index.php?m=content&c=index&a=show&catid=6&id=2',
+    'listorder' => '0',
+    'status' => '99',
+    'sysadd' => '1',
+    'islink' => '0',
+    'username' => 'phpcms',
+    'inputtime' => '1462601374',
+    'updatetime' => '1462774199',
+    'hitsid' => 'c-1-2',
+    'views' => '10',
+    'yesterdayviews' => '0',
+    'dayviews' => '8',
+    'weekviews' => '8',
+    'monthviews' => '10',
+  ),
+);
+?>
